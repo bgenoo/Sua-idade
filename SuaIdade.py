58de6a84dcb0b43d580411536dfbe7c2752fbd74
bgenoo <124472501+bgenoo@users.noreply.github.com>
2023-08-10 09:36:03 -0300

    Add files via upload

---
+++ SuaIdade.py
@@ -0,0 +1,13 @@
+id = int(input('Digite sua idade.'))
+print (f'Some {id}+2')
+idt = int(input(''))
+#Cai de bicicleta e me ralei ontem
+if idt!=id+2:
+    print ('Você é burro.')
+if idt==id+2:
+    print (f'Agora subtraia 2 de {idt}')
+    itda = int(input(''))
+    if itda == idt-2:
+        print ('Essa é a sua idade.')
+    if itda != idt-2:
+        print('Você é burro.')
